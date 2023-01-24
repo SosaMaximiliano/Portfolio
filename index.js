84b8aa29c374b903a8af2e9af97362e52cfa2925
Maximiliano Sosa <68975256+SosaMaximiliano@users.noreply.github.com>
2023-01-23 22:11:58 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -13,4 +13,54 @@ izquierda.addEventListener('mouseout' ,()=>{
     izquierda.querySelector("hr").style = "width:20%";
 });
 
-derecha.querySelector("#experiencia").addEventListener('mouseover')
+
+//NOMBRE
+derecha.querySelector("#datos").addEventListener('mouseover', ()=>{
+    derecha.querySelector("#hr-nombre").style = "width:400px; opacity:1";
+    derecha.querySelector("#datos").style = "background: linear-gradient(90deg, rgba(240,191,134,1) 0%, rgba(255,228,196,1) 100%)";
+
+});
+
+derecha.querySelector("#datos").addEventListener('mouseout', ()=>{
+    derecha.querySelector("#hr-nombre").style = "width: 0px; opacity:0";
+    derecha.querySelector("#datos").style = "background: rgba(255,228,196,1)";
+
+});
+
+
+//EXPERIENCIA
+derecha.querySelector("#experiencia").addEventListener('mouseover', ()=>{
+    derecha.querySelector("#hr-experiencia").style = "width:260px; opacity:1";
+    derecha.querySelector("#experiencia").style = "background: linear-gradient(90deg, rgba(240,191,134,1) 0%, rgba(255,228,196,1) 100%)";
+});
+
+derecha.querySelector("#experiencia").addEventListener('mouseout', ()=>{
+    derecha.querySelector("#hr-experiencia").style = "width: 0px; opacity:0";
+    derecha.querySelector("#experiencia").style = "background: rgba(255,228,196,1)";
+});
+
+
+//EDUCACION
+derecha.querySelector("#educacion").addEventListener('mouseover', ()=>{
+    derecha.querySelector("#hr-educacion").style = "width:135px; opacity:1";
+    derecha.querySelector("#educacion").style = "background: linear-gradient(90deg, rgba(240,191,134,1) 0%, rgba(255,228,196,1) 100%)";
+});
+
+derecha.querySelector("#educacion").addEventListener('mouseout', ()=>{
+    derecha.querySelector("#hr-educacion").style = "width: 0px; opacity:0";
+    derecha.querySelector("#educacion").style = "background: rgba(255,228,196,1)";
+
+});
+
+
+//IDIOMAS
+derecha.querySelector("#idiomas").addEventListener('mouseover', ()=>{
+    derecha.querySelector("#hr-idiomas").style = "width:100px; opacity:1";
+    derecha.querySelector("#idiomas").style = "background: linear-gradient(90deg, rgba(240,191,134,1) 0%, rgba(255,228,196,1) 100%)";
+});
+
+derecha.querySelector("#idiomas").addEventListener('mouseout', ()=>{
+    derecha.querySelector("#hr-idiomas").style = "width: 0px; opacity:0";
+    derecha.querySelector("#idiomas").style = "background: rgba(255,228,196,1)";
+
+});
